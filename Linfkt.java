c15e2d346cadd20e8a54c94c520313b0691e0b2c
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-02-06 16:28:54 +0100

    initialcommit

---
+++ Linfkt.java
@@ -0,0 +1,43 @@
+/**
+Beschreiben Sie hier die Klasse Linfkt.
+
+@author (Ihr Name)
+@Version (eine Versionsnummer oder ein Datum)
+*/
+public class Linfkt
+{
+// Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+private int x;
+public double steigung;
+public double psteigung;
+public double yAchsenAbschnitt;
+public double pyAchsenAbschnitt;
+public double xWert;
+public double yWert;
+public double nullStelle;
+/**
+* Konstruktor für Objekte der Klasse KarstenStahl
+
+* */
+public Linfkt(double steigung, double yAchsenabschnitt)
+{
+psteigung = steigung;
+pyAchsenAbschnitt = yAchsenabschnitt;
+}
+/**
+*
+Diese Methode berechnet y-Werte
+
+*/
+public final void yWerteBerechnen(double xWert)
+{
+   
+yWert = psteigung * xWert + pyAchsenAbschnitt; //Werte berechnen   
+
+System.out.println(yWert);//Werte ausgeben 
+    
+}
+
+}
+
+
