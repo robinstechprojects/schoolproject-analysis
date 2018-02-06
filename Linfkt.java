ddcef31eb3ea7ba1f26967dc3b55dadd7b7a58dd
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-02-06 16:48:02 +0100

    Nullstellen berechnen.... #1

--- Linfkt.java
+++ Linfkt.java
@@ -7,14 +7,17 @@ Beschreiben Sie hier die Klasse Linfkt.
 public class Linfkt
 {
 // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-private int x;
+
 public double steigung;
 public double psteigung;
 public double yAchsenAbschnitt;
 public double pyAchsenAbschnitt;
 public double xWert;
+public double x;
 public double yWert;
 public double nullStelle;
+public double schritt1;
+public double schritt2;
 /**
 * Konstruktor für Objekte der Klasse KarstenStahl
 
@@ -35,6 +38,12 @@ public final void yWerteBerechnen(double xWert)
 yWert = psteigung * xWert + pyAchsenAbschnitt; //Werte berechnen   
 
 System.out.println(yWert);//Werte ausgeben 
+    
+}
+
+public final void nullStelleBerechnen(){
+
+    System.out.println("commingSoon");
     
 }
 
